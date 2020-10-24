d63288b96529b63854b78b836383fefebb1df10b
cihangirkoral <cinarkoral@gmail.com>
2020-10-24 14:17:08 +0200

    first string method file created

---
+++ string.py
@@ -0,0 +1,16 @@
+print ("helloboiii")
+name = "Cihangir KORAL"
+u_name = name.upper()
+
+##################### S T R I N G   M E T H O D S ######################
+
+# title method
+print (name.title())
+
+# Upper and lower method
+print (u_name)
+print (name.lower())
+
+
+
+
