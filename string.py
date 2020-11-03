a75802027cb57be2f81f4c5e1f77dc04e5e1f051
cihangirkoral <cinarkoral@gmail.com>
2020-11-03 21:20:48 +0100

    newest string methods!!!

--- string.py
+++ string.py
@@ -1,16 +1,30 @@
-print ("helloboiii")
-name = "Cihangir KORAL"
-u_name = name.upper()
+name = "Cihangir Koral"
 
-##################### S T R I N G   M E T H O D S ######################
+print ("name:" , name)
 
-# title method
-print (name.title())
+print (name.capitalize())
 
-# Upper and lower method
-print (u_name)
-print (name.lower())
+name_title = name.title()
+
+name_cap = name.capitalize()
+
+print ("name_title:" , name_title)
+
+print ("name_cap" , name_cap)
 
 
 
+# # concanatation
+
+first_name = "Cihangir"
+
+last_name = "Koral"
+
+full_name = first_name + " " + last_name
+
+# new line and tab.
+
+print ("full name:" , full_name)
+
+print (full_name + "\nis a good boiii \n\the loves his fatherrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr")
 
