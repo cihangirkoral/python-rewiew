f08018b6626a43d9529fa808b062cd5c6a2f008f
cihangirkoral <cinarkoral@gmail.com>
2020-11-05 17:31:09 +0100

    lists.py eklendi.

---
+++ lists.py
@@ -0,0 +1,15 @@
+# square bracket
+# data collection
+
+
+# string list
+my_list = ["ekmek", "baba", "kaşar", "taktak"]
+
+print (my_list)
+
+my_element = my_list[0]
+
+print (my_element)
+
+# Ekrana ne yazar?
+print (my_element == my_list[-4])
