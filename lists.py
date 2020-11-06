1ea3a00f976e441f69c581448dfd7b2a93d33110
cihangirkoral <cinarkoral@gmail.com>
2020-11-06 19:19:34 +0100

    lists updated

--- lists.py
+++ lists.py
@@ -13,3 +13,22 @@ print (my_element)
 
 # Ekrana ne yazar?
 print (my_element == my_list[-4])
+
+
+
+
+##############################  L İ S T  S H O W S  A D D R E S S  I N  M E M O R Y  ############################
+
+my_list = ["ankara" , "bursa" , "ceyhan"]
+
+your_list = my_list
+
+your_list[0] = "adana"
+
+new_list = my_list
+
+new_list = ["a" , "b" , "c"]
+
+print (your_list[0].upper())
+
+
