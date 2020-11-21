4eb95586728a8959a3b3c6962aa9a42b0e47445d
cihangirkoral <cinarkoral@gmail.com>
2020-11-21 19:27:49 +0100

    new homework added

--- homeworks/homework_04.py
+++ homeworks/homework_04.py
@@ -24,4 +24,4 @@
 #   •	 Prove that you have two separate lists Print the message, My favorite
 #           pizzas are:, and then use a for loop to print the first list Print the message,
 #           My friend’s favorite pizzas are:, and then use a for loop to print the second list
-#           Make sure each new pizza is stored in the appropriate list
+#           Make sure each new pizza is stored in the appropriate listz
