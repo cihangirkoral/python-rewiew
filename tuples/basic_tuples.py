a795b76b3e332d147e19d7f51807a47adc67403a
cihangirkoral <cinarkoral@gmail.com>
2020-12-05 12:31:18 +0100

    Tuples recap added

--- tuples/basic_tuples.py
+++ tuples/basic_tuples.py
@@ -17,3 +17,47 @@ for number in numbers_t:
 new_tuple = numbers_t[:]
 
 new_list = list(numbers_t)
+
+################################################################
+################### TUPLE RECAP ################################
+################################################################
+
+empty_tuple = ()
+
+countries = ('Belgium', 'Germany', 'France', 'Netherland', 'Poland')
+print(countries)
+
+birthyears = (1985, 1986, 1988, 1989, 1999)
+print(birthyears)
+
+that_man = (42, 'smart', 1.70, True, ['tennis', 'walking', 'ski'])
+print(that_man)
+
+sport = that_man[4][0]
+print(sport)
+
+print('I like', sport)
+
+print(countries[1:3])
+
+# print(countries[3:])
+
+print(countries[::-1])
+
+
+# Below code gives us an error.
+# countries[-1] = 'Hi'
+
+# I can change whole tuple
+countries = ('Spain', 'Sweden', 'Italy', 'Denmark', 'Norway')
+
+check_if_exist = 'Belgium' in countries
+print(type(check_if_exist))
+
+
+print(str(check_if_exist) + '\n')
+
+for country in countries:
+    print(country)
+
+del countries
