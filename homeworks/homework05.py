47715ebc7bf0547056ff17007f9c0c4e8d818342
cihangirkoral <cinarkoral@gmail.com>
2020-12-06 18:15:38 +0100

    homework completed

--- homeworks/homework05.py
+++ homeworks/homework05.py
@@ -1,4 +1,3 @@
-  
 # Below pizzas are sold in the pizza restaurant
 pizzas = ['margaritha', 'pepperoni', 'salami',
           'beefking', 'meatball', 'mozarella']
@@ -8,7 +7,7 @@ my_favorites = ['potatomi', 'kaasprince',
                 'margaritha', 'beefking', 'mozarella']
 
 
-# Write a program chek all available pizzas in store
+# Write a program check all available pizzas in store
 # And then compare favorite pizzas.
 # if it matches, add to my_cart list object
 # At the end of the program print my_cart list.
@@ -16,3 +15,14 @@ my_favorites = ['potatomi', 'kaasprince',
 # Hints:
 # Use for
 # use if
+
+######### ANSWER #########
+
+my_cart = []
+
+for my_favorite in my_favorites:
+    for pizza in pizzas:
+        if pizza == my_favorite:
+            my_cart.append(pizza)
+
+print(my_cart)
