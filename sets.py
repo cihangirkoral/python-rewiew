cdd7a3f6d1cd54a7b1047765bba8bfb3d8c0b7cd
cihangirkoral <cinarkoral@gmail.com>
2020-12-20 18:45:54 +0100

    funtions and a homework added

---
+++ sets.py
@@ -0,0 +1,19 @@
+### An empty list ###
+empty_set = set()
+
+### strings ###
+countries= {'Belgium', 'Germany', 'France', 'Netherland', 'Poland'}
+print(countries)
+
+### integers ### 
+birthyears= {1985,1986,1988,1989,1999}
+print(birthyears)
+
+### mixed ###
+that_man = {42, 'smart', 1.70, True, ('tennis', 'walking')}
+print(that_man)
+
+### adding an item ###
+birthyears.add(2000)
+print(birthyears)
+
