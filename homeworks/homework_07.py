926c2327308055a940af287b9dfc4a69d66663df
cihangirkoral <cinarkoral@gmail.com>
2020-12-30 13:59:19 +0100

    homework completed with more stuff

--- homeworks/homework_07.py
+++ homeworks/homework_07.py
@@ -6,3 +6,27 @@
 # your program should include the tax amount, the tip amount, and the grand total for
 # the meal including both the tax and the tip. Format the output so that all of the values
 # are displayed using two decimal places and € currency.
+
+while True:
+    try:
+        cost_meal = float(input ("How much did the meal cost?(!!in euros pls!!)"))
+        break
+
+    except ValueError:
+        cost_meal = input ("please give a number!(!!in euros pls!!)")
+        continue
+    
+
+
+tax = 8 / 100 * cost_meal
+tax = ("%.2f" % tax)
+
+tip = 18 / 100 * cost_meal
+tip = ("%.2f" % tip)
+
+total = float(tax) + cost_meal + float(tip)
+total = ("%.2f" % total)
+
+print ("Tax: ", "€", tax)
+print ("Tip: ", "€", tip)
+print ("Total: ", "€", total)
